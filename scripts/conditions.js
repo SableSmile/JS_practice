2633a1801f0da32d9f7a50c74ef1d61c0bf287a4
SableSmile <32065905+SableSmile@users.noreply.github.com>
2021-08-31 16:39:08 +0800

    add new js test script

---
+++ scripts/conditions.js
@@ -0,0 +1,17 @@
+const pet='cat';
+
+if (pet!=='cat'){
+    alert('It is not a cat!');
+}
+else {
+    alert('Meow~');
+    console.log(pet);
+}
+
+console.log(add(20,0.5));
+
+function add(num1,num2){
+    const sum = num1 + num2 ;
+    return sum;
+}
+
