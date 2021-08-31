498c54ad23d7b8e821e32a7a47dabec2c8e15a3c
SableSmile <32065905+SableSmile@users.noreply.github.com>
2021-08-31 16:10:25 +0800

    0831 first commit

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+const a = 1;
+console.log(a);
+console.log(2);
+
+
+//const t1Msg = document.querySelector('h1');
+//t1Msg.textContent = 'JS';
+document.querySelector('h1').textContent = 'JS';
+
+const textMsg = document.querySelector('h2');
+textMsg.textContent = 'Hello world! Used by javascript';
+
+const titleMsg = document.querySelector('#title');
+titleMsg.textContent = 'WOW';
